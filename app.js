b07c7119874ab0eb3b56c1f6b2e7dc0a8ce008cf
arjun9435 <arjun9435@gmail.com>
2016-09-16 11:48:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,58 +1,36 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.use(express.bodyParser());
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.get('/', function(req, res) {
+  console.log(req);
+  res.send('It works!');
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+app.get(['/facebook', '/instagram'], function(req, res) {
+  if (
+    req.param('hub.mode') == 'subscribe' &&
+    req.param('hub.verify_token') == 'databeat_token'
+  ) {
+    res.send(req.param('hub.challenge'));
+  } else {
+    res.send(400);
+  }
 });
 
+app.post('/facebook', function(req, res) {
+  console.log('Facebook request body:');
+  console.log(req.body);
+  // Process the Facebook updates here
+  res.send(200);
+});
 
-module.exports = app;
+app.post('/instagram', function(req, res) {
+  console.log('Instagram request body:');
+  console.log(req.body);
+  // Process the Instagram updates here
+  res.send(200);
+});
+
+app.listen(101);
